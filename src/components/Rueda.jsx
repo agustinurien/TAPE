7212fcad07ad302e222899fa8985a843a2d01e87
Tommy Almeida <tommyalmeida@Sin-sitcker.local>
2025-06-04 17:53:12 -0300

    home

--- src/components/Rueda.jsx
+++ src/components/Rueda.jsx
@@ -7,25 +7,31 @@ const Rueda = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY;
-      setRotation(scrollY * 0.5); // podés ajustar el factor
+      setRotation(scrollY * 0.3); // podés ajustar el factor
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <div className="rueda-container">
       <img
         src={rueda.src}
-        width={150}
-        height={150}
+        width={1050}
+        height={1050}
         alt=""
         style={{
+          paddingRight: 3,
+          position: "absolute",
+          top: 0,
+          right: 0,
+          width: "100%",
+          height: "100%",
+          scale: 0.7,
+          objectFit: "contain",
           transform: `rotate(${rotation}deg)`,
           transition: "transform 0.1s linear", // suaviza el giro
         }}
       />
-    </div>
   );
 };
 
