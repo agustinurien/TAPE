08254b45d7ec2a3ca2ecdbab1bb6f2e912bcece4
Agustín Urien <agus.urien2@gmail.com>
2025-06-05 17:36:02 -0300

    hasta quienes somos terminado

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./portfolio.css";
+import frenteImage from "../../assets/portfolio1@300x.png";
+import portfolio2 from "../../assets/portfolio2.png";
+import portfolio3 from "../../assets/portfolio3.png";
+import portfolio4 from "../../assets/portfolio4.png";
+import portfolio5 from "../../assets/portfolio1.png";
+import cassete from "../../assets/cassete.png";
+const Portfolio = () => {
+  return (
+    <div className="portfolio">
+      <h2 className="titlePortfolio">
+        Nuestros <strong>trabajos</strong> y <strong>clientes</strong>.
+      </h2>
+      <div className="portfolio__container">
+        <div className="cassete">
+          <img src={cassete.src} alt="cassete" className="image" />
+        </div>
+        <div className="fondo">
+          <div>
+            <img src={portfolio5.src} alt="" className="image" />
+          </div>
+          <div>
+            <img src={portfolio3.src} alt="" className="image" />
+          </div>
+        </div>
+
+        <div className="medio">
+          <div>
+            <img src={portfolio4.src} alt="" className="image" />
+          </div>
+          <div>
+            <img src={portfolio2.src} alt="" className="image" />
+          </div>
+        </div>
+
+        <div className="frente">
+          <div className="frenteImage">
+            <img src={frenteImage.src} alt="" className="image" />
+          </div>
+        </div>
+      </div>
+      <button className="portfolioButton">VER PORTFOLIO</button>
+    </div>
+  );
+};
+
+export default Portfolio;
