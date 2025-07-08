2e855a4e57640382910be130c894c0799ecb7792
Agustín <agustin@MacBook-Pro-de-MacBookPro.local>
2025-07-08 17:33:45 -0300

    terminado falta responsive

--- src/components/PortfolioIslands.jsx
+++ src/components/PortfolioIslands.jsx
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
 import "./portfolioIslands.css";
 
-import dinko from "../assets/proyectos/dinko.png";
-import global from "../assets/proyectos/global.png";
-import jpr from "../assets/proyectos/jpr.png";
-import wakas from "../assets/proyectos/wakas.png";
-import bold from "../assets/proyectos/bold.png";
-import urda from "../assets/proyectos/urda.png";
-import market from "../assets/proyectos/market.png";
-import marlom from "../assets/proyectos/marlom.png";
-import cobarde from "../assets/proyectos/cobarde.png";
+import dinko from "../assets/proyectos/dinko.jpg";
+import global from "../assets/proyectos/global.jpg";
+import jpr from "../assets/proyectos/jpr.jpg";
+import wakas from "../assets/proyectos/wakas.jpg";
+import bold from "../assets/proyectos/bold.jpg";
+import urda from "../assets/proyectos/urdapilleta.jpg";
+import market from "../assets/proyectos/marketbay.jpg";
+import marlom from "../assets/proyectos/marloms.jpg";
+import cobarde from "../assets/proyectos/cobarde.jpg";
+import acequia from "../assets/proyectos/acequia.jpg";
+import capital from "../assets/proyectos/capital.jpg";
+import amalia from "../assets/proyectos/amalia.jpg";
+import jpu from "../assets/proyectos/jpu.jpg";
+import calello from "../assets/proyectos/calello.jpg";
+import refmaster from "../assets/proyectos/REFMASTER.jpg";
+import martin from "../assets/proyectos/martin.jpg";
+import garabi from "../assets/proyectos/garabi.jpg";
+import pantea from "../assets/proyectos/pantea.jpg";
+import santos from "../assets/proyectos/santos.jpg";
+import trikids from "../assets/proyectos/trikids.jpg";
+
+import BOLD from "../../public/manuales/BOLD.pdf";
+import COBARDE from "../../public/manuales/COBARDE.pdf";
+import DINKO from "../../public/manuales/DINKO.pdf";
+import GLOBAL from "../../public/manuales/GLOBAL.pdf";
+import JPR from "../../public/manuales/JPR.pdf";
+import REFMASTER from "../../public/manuales/REFMASTER.pdf";
+import URDAPILLETA from "../../public/manuales/URDAPILLETA.pdf";
+import MARKBAY from "../../public/manuales/MARKBAY.pdf";
+
+import { FaGlobe, FaInstagram } from "react-icons/fa";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 const PortfolioIslands = () => {
   const [active, setActive] = useState("");
@@ -25,97 +48,135 @@ const PortfolioIslands = () => {
         "logo",
         "fotografia",
       ],
+      linkDescarga: DINKO,
+      linkWeb: "https://www.hoteldinko.com.ar/",
+      linkRed: "https://www.instagram.com/dinkohotel/?hl=es",
     },
+
     {
       title: "Global Red de Inmobiliarias",
       portada: global,
       servicios: ["sitios web", "identidad de marca", "redes sociales", "logo"],
+      linkDescarga: GLOBAL,
+      linkWeb: "https://www.globalreddeinmobiliarias.com/",
+      linkRed: "https://www.instagram.com/globalreddeinmobiliarias/",
     },
     {
       title: "JPR",
       portada: jpr,
       servicios: ["sitios web", "identidad de marca", "logo"],
+      linkDescarga: JPR,
+      linkWeb: "https://jpr-web-plum.vercel.app/",
     },
     {
       title: "Bold Real Estate",
       portada: bold,
       servicios: ["sitios web", "identidad de marca", "redes sociales"],
+      linkDescarga: BOLD,
+      linkWeb: "https://boldrealestate.com.ar/",
+      linkRed: "https://www.instagram.com/bold_real_estate/",
     },
     {
       title: "Wakas Gluten Free",
       portada: wakas,
       servicios: ["sitios web", "redes sociales", "fotografia"],
+      linkWeb: "https://wakas.com.ar/",
+      linkRed: "https://www.instagram.com/wakasglutenfree/",
     },
     {
       title: "Marlom's",
       portada: marlom,
       servicios: ["sitios web", "redes sociales", "fotografia"],
+      linkWeb: "https://marloms.com.ar/",
+      linkRed: "https://www.instagram.com/marlomsargentina/",
     },
     {
       title: "Cobarde",
       portada: cobarde,
-      servicios: ["sitios web", "redes sociales", "logo"],
+      servicios: ["sitios web", "logo", "identidad de marca"],
+      linkDescarga: COBARDE,
+      linkWeb: "https://cobarde.com.ar/",
     },
     {
       title: "Acequia Casa de Vinos",
-      portada: "",
+      portada: acequia,
       servicios: ["sitios web", "redes sociales"],
+      linkWeb: "https://acequiacasadevinos.com.ar/",
+      linkRed: "https://www.instagram.com/acequiacasadevinos/",
     },
     {
       title: "Urdapilleta Propiedades",
       portada: urda,
       servicios: ["identidad de marca", "redes sociales"],
+      linkDescarga: URDAPILLETA,
+      linkRed: "https://www.instagram.com/urdapilleta.propiedades/",
     },
     {
       title: "Capital Agribuisiness",
-      portada: "",
+      portada: capital,
       servicios: ["sitios web", "logo"],
+      linkWeb: "https://capitalagribuisness.vercel.app/",
     },
     {
       title: "Amalia Cavagnagh",
-      portada: "",
+      portada: amalia,
       servicios: ["sitios web", "logo"],
+      linkWeb: "/",
     },
     {
       title: "Marketbay",
       portada: market,
       servicios: ["identidad de marca", "logo"],
+      linkDescarga: MARKBAY,
+    },
+    {
+      title: "Refmaster",
+      portada: refmaster,
+      servicios: ["identidad de marca"],
+      linkDescarga: REFMASTER,
     },
 
     {
       title: "Santiago Calello",
-      portada: "",
+      portada: calello,
       servicios: ["sitios web"],
+      linkWeb: "https://calello.vercel.app/",
     },
     {
       title: "Martin Cesarsky",
-      portada: "",
+      portada: martin,
       servicios: ["sitios web"],
+      linkWeb: "https://www.martincesarsky.com.ar/",
     },
     {
       title: "Garabí Casas",
-      portada: "",
+      portada: garabi,
       servicios: ["redes sociales"],
+      linkRed: "https://www.instagram.com/garabicasas/",
     },
     {
       title: "Pantea Smart House",
-      portada: "",
+      portada: pantea,
       servicios: ["redes sociales"],
+      linkRed: "https://www.instagram.com/panteasmarthouse/",
     },
     {
       title: "Santos Mieles",
-      portada: "",
+      portada: santos,
       servicios: ["redes sociales"],
+      linkRed: "https://www.instagram.com/mielessantos_/",
     },
     {
       title: "Trikids",
-      portada: "",
+      portada: trikids,
       servicios: ["redes sociales"],
+      linkRed: "https://www.instagram.com/trikidsbikes/",
     },
     {
       title: "JPU",
-      portada: "",
+      portada: jpu,
       servicios: ["fotografia"],
+      linkRed: "https://www.instagram.com/jpudesarrollos/",
     },
   ];
 
@@ -173,6 +234,32 @@ const PortfolioIslands = () => {
             )}
             <div className="proyectoTitulo">
               <h2>{proyecto.title}</h2>
+              <div style={{ display: "flex", gap: "4px", paddingRight: 10 }}>
+                {proyecto.linkDescarga && (
+                  <div className="descargar">
+                    <a
+                      href={proyecto.linkDescarga}
+                      download={proyecto.linkDescarga}
+                      className="descargarLink"
+                    >
+                      Descargar manual de marca
+                    </a>
+                    <a href={proyecto.linkDescarga}>
+                      <MdOutlineFileDownload fontSize={22} />
+                    </a>
+                  </div>
+                )}
+                {proyecto.linkWeb && (
+                  <a href={proyecto.linkWeb} target="_blank" className="web">
+                    <FaGlobe />
+                  </a>
+                )}
+                {proyecto.linkRed && (
+                  <a href={proyecto.linkRed} target="_blank" className="insta">
+                    <FaInstagram />
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         ))}
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+import logo from "../../assets/logo2Faq.png";
 import "./form.css";
 const Form = () => {
-  const [showForm, setShowForm] = useState(true);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     const handleOpen = () => setShowForm(true);
@@ -13,65 +14,128 @@ const Form = () => {
     showForm && (
       <div className="formContainer">
         <div className="close">
-          <button onClick={() => setShowForm(false)}>cerrar</button>
+          <button onClick={() => setShowForm(false)}>
+            CERRAR <strong>X</strong>
+          </button>
         </div>
 
         <form>
           <div className="formContent">
             <div className="campo nombre">
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <div className="nombreContent">
                 <label htmlFor="name">¡Hola! Mi nombre es</label>
-                <input type="text" id="name" name="name" required />
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Tu Nombre"
+                  required
+                />
               </div>
 
               <h2>,</h2>
             </div>
 
             <div className="campo profesion">
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <div className="profesionContent">
                 <label htmlFor="email">y soy</label>
-                <input type="email" id="email" name="email" required />
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="(Fundador de la marca / Responsable de Marketing / Emprendedor, etc.)"
+                  required
+                />
               </div>
               <h2>.</h2>
             </div>
 
             <div className="campo razon">
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <div className="razonContent">
                 <label htmlFor="email">
                   Me pongo en contacto con ustedes por
                 </label>
-                <input type="email" id="email" name="email" required />
+                <div className="selectContenedor">
+                  <h2>(</h2>
+                  <select id="profesion" name="profesion" required>
+                    <option value="" disabled selected>
+                      Seleccionar
+                    </option>
+                    <option value="desarrollador">Desarrollador</option>
+                    <option value="diseñador">Diseñador</option>
+                    <option value="artista">Artista</option>
+                    <option value="otro">Otro</option>
+                  </select>
+                  <h2>)</h2>
+                </div>
               </div>
               <h2>.</h2>
             </div>
 
             <div className="campo llegue">
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <div className="llegueContent">
                 <label htmlFor="email">Llegué a TAPE a través de</label>
-                <input type="email" id="email" name="email" required />
+                <div className="selectContenedor">
+                  <h2>(</h2>
+                  <select id="profesion" name="profesion" required>
+                    <option value="" disabled selected>
+                      Seleccionar
+                    </option>
+                    <option value="desarrollador">Desarrollador</option>
+                    <option value="diseñador">Diseñador</option>
+                    <option value="artista">Artista</option>
+                    <option value="otro">Otro</option>
+                  </select>
+                  <h2>)</h2>
+                </div>
               </div>
               <h2>.</h2>
             </div>
 
             <div className="campo resumen">
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <div className="resumenContent">
                 <label htmlFor="email">En resumen</label>
-                <textarea name="" id=""></textarea>
+                <textarea
+                  id="resumen"
+                  rows="4"
+                  placeholder="Contanos brevemente de qué se trata tu proyecto"
+                ></textarea>
               </div>
             </div>
 
             <div className="campo datos">
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <div className="datosContent">
                 <label htmlFor="email">Mis datos de contacto</label>
-                <input type="email" id="email" name="email" required />
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Tu email (obligatorio)"
+                  required
+                />
               </div>
               <h2>,</h2>
             </div>
 
             <div className="campo email">
-              <input type="email" id="email" name="email" required />
-              <h2>,</h2>
-              <input type="email" id="email" name="email" required />
+              <div className="emailContent">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Tu número de celular (opcional)"
+                  required
+                />
+                <h2>,</h2>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Ciudad (opcional)"
+                  required
+                />
+              </div>
+              <h2>.</h2>
             </div>
 
             <div className="campo saludo">
@@ -79,8 +143,24 @@ const Form = () => {
             </div>
           </div>
           <div className="campo submit">
-            <div className="foto"></div>
-            <button type="submit">Enviar</button>
+            <div className="foto">
+              <img src={logo.src} alt="logo" />
+            </div>
+            <div className="contacto">
+              <h3 style={{ marginBottom: 20 }}>
+                Direccion: San Isidro - Buenos Aires
+              </h3>
+              <h3>
+                Email: <strong>hola@tapeproducciones.com</strong>
+              </h3>
+              <h3>
+                Instagram: <strong>@tapeproducciones</strong>
+              </h3>
+              <div></div>
+            </div>
+            <div className="buttonEnviar">
+              <button type="submit">ENVIAR</button>
+            </div>
           </div>
         </form>
       </div>
