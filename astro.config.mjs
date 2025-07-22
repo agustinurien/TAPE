10117a572f8546535768d38ddd654731986fa51c
Agustin Urien <agus.urien2@gmail.com>
2025-07-22 14:17:00 -0300

    vercel server config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercelAdapter(),
   integrations: [react()],
+
 });
